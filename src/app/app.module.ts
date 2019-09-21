5282fabd77ca748abc7ace3b7df3ff789f72a552
Javier Gamarra <nhpatt@gmail.com>
2019-09-21 16:54:34 +0200

    add new example component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SolicitudComponent } from './solicitud/solicitud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolicitudComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/solicitud/solicitud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud',
+  templateUrl: './solicitud.component.html',
+  styleUrls: ['./solicitud.component.css']
+})
+export class SolicitudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
