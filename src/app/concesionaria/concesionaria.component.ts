25ec8c0b9efb0873518aab4b6e7e97923007f260
Javier Gamarra <nhpatt@gmail.com>
2019-09-25 23:02:54 +0200

    break everything removing css encapsulation

--- src/app/concesionaria/concesionaria.component.ts
+++ src/app/concesionaria/concesionaria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {
   AngularFirestore,
   AngularFirestoreCollection
@@ -39,6 +39,7 @@ export interface Concesionaria {
       </div>
     </div>
   `,
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./concesionaria.component.css']
 })
 export class ConcesionariaComponent implements OnInit {
