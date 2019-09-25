6355a05fccc71dffc9c930d04d70d8130e0be234
Javier Gamarra <nhpatt@gmail.com>
2019-09-25 07:57:39 +0200

    add e2e test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getParagraphText() {
+    return element(by.css('app-root')).getText();
   }
 }
